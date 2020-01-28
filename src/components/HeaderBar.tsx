017f801e308db80e781fd97000a959f2b2088e7f
Paul Kamer <paulkamer@gmail.com>
2020-01-28 20:45:27 +0100

    Restyled HeaderBar
    added links to Home & Projects pages to it

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -6,12 +6,23 @@ import './HeaderBar.scss';
 const HeaderBar = () => {
   return (
     <header className="HeaderBar">
-        <Link to="/">
-          <img className="HeaderBar avatar" alt="avatar" src="/avatar192.jpg" />
-        </Link>
-        <Link to="/">
-          <h1>Paul Kamer</h1>
-        </Link>
+      <div className="HeaderBar__container">
+        <div className="HeaderBar__name">
+          <Link to="/">
+              <img className="HeaderBar avatar" alt="avatar" src="/avatar192.jpg" />
+            </Link>
+            <Link to="/">
+              <h1>Paul Kamer</h1>
+            </Link>
+        </div>
+
+        <div className="HeaderBar__menu">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/projects/">Projects</Link></li>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 }
