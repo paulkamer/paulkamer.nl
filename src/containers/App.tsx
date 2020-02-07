5f4effbefe4d796e27f79e344109035de544b158
Paul Kamer <paulkamer@gmail.com>
2020-02-07 20:44:46 +0100

    Lazy-load project pages
    To reduce bundle size

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { Suspense }  from 'react';
 import { BrowserRouter as Router, Switch,  Route } from 'react-router-dom';
 
 import Home from './Home';
 import HeaderBar from '../components/HeaderBar';
 import Submenu from '../components/Submenu';
 import FooterBar from '../components/FooterBar';
-
-import ProjectsIndexPage from './projects/ProjectsIndexPage';
-import ProjectWorldfootballPage from './projects/ProjectWorldfootballPage';
-import ClinicaltrialsmonitorPage from './projects/ProjectTrialmonitorPage';
-
+import Loader from '../components/Loader';
 
 import '../styles/variables.scss';
 import './App.scss';
+
+const ProjectsIndexPage = React.lazy(() => import('./projects/ProjectsIndexPage'));
+const ProjectWorldfootballPage = React.lazy(() => import('./projects/ProjectWorldfootballPage'));
+const ClinicaltrialsmonitorPage = React.lazy(() => import('./projects/ProjectTrialmonitorPage'));
 
 function App() {
   return (
@@ -25,12 +25,13 @@ function App() {
         </div>
         <Switch>
           <Route>
-
             <div className="app-main__page-container">
-              <Route exact path="/" component={Home} />
-              <Route exact path="/projects/worldfootball/" component={ProjectWorldfootballPage} />
-              <Route exact path="/projects/trialmonitor/" component={ClinicaltrialsmonitorPage} />
-              <Route exact path="/projects/" component={ProjectsIndexPage} />
+              <Suspense fallback={<Loader />}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/projects/worldfootball/" component={ProjectWorldfootballPage} />
+                <Route exact path="/projects/trialmonitor/" component={ClinicaltrialsmonitorPage} />
+                <Route exact path="/projects/" component={ProjectsIndexPage} />
+              </Suspense>
             </div>
           </Route>
         </Switch>
