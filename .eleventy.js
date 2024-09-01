57680a4dbbd9f231f9c0b4ce5a889d954871db8f
Paul Kamer <paulkamer@gmail.com>
2024-09-01 11:26:53 +0200

    update deployment script

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,15 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/assets/images");
-    eleventyConfig.addPassthroughCopy({"src/assets/docs": "docs"});
-    eleventyConfig.addPassthroughCopy({"./src/site.webmanifest": "site.webmanifest"});
+  eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy({"src/assets/docs": "docs"});
+  eleventyConfig.addPassthroughCopy({"./src/site.webmanifest": "site.webmanifest"});
   
-    return {
-      dir: {
-        input: "src",
-        data: "_data",
-        includes: "_includes",
-        layouts: "_layouts",
-      },
-    };
+  return {
+    dir: {
+      input: "src",
+      data: "_data",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
   };
+};
